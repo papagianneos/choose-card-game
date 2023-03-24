6461cec1147a8de541ecd4b670ede6ced1a1e0a0
Sotiris25 <71824952+Sotiris25@users.noreply.github.com>
2023-03-24 13:50:52 +0200

    Add files via upload

---
+++ modules/specialCardsConfig.js
@@ -0,0 +1,57 @@
+// Για τις σπεσιαλ κάρτες (πληροφορίες για setup)
+export const specialCardsConfig = [
+    { // +10 Score
+        shape: '++',
+        color: 'radial-gradient(darkgreen, white)',
+        info: '+10 Score'
+    },
+
+    { // Διπλό Score
+        shape: 'x2',
+        color: 'radial-gradient(#8a8c16, #8a8c16, gold)',
+        info: 'Διπλό Score'
+    },
+
+    { // Μισό Score
+        shape: '¹/₂',
+        color: 'radial-gradient(cyan, red)',
+        info: 'Μισό Score'
+    },
+
+    { // 2 λιγότερες προσπάθειες
+        shape: 'T-2',
+        color: 'radial-gradient(#00fc82, #84b89f)',
+        info: '2 Λιγότερες Προσπάθειες'
+    },
+
+    { // -10 Score
+        shape: '--',
+        color: 'radial-gradient(black, #4a1313)',
+        info: '-10 Score'
+    },
+
+    { // Πάει χάθηκε το παιχνίδι
+        shape: '†',
+        color: 'radial-gradient(#1c0b0e, #b8707d)',
+        info: 'Πάει έχασες το παιχνίδι'
+    },
+
+    { // Ουάου
+        shape: 'Pgn',
+        color: 'radial-gradient(#adfff1, #265175)',
+        info: 'PAPAGIANNEOS SPEECH'
+    },
+
+    { // friend :)
+        shape: 'Κ',
+        color: 'radial-gradient(#ac86b0, #781f82)',
+        info: '???'
+    },
+
+    { // kaboom
+        shape: '▲',
+        color: 'radial-gradient(orange, red)',
+        info: 'ΠΡΟΣΟΧΗ EΚΡΗΞΗ',
+        fatSymbol: true
+    }
+];
