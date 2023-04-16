54e60f8735c0adfda6358628b642b2257957a20f
Sotiris25 <71824952+Sotiris25@users.noreply.github.com>
2023-04-16 21:50:06 +0300

    Add files via upload

---
+++ modules/featured-youtuber.js
@@ -0,0 +1,26 @@
+export const FEATURED_YOUTUBERS = [
+    {
+        name: 'Engood',
+        link: 'https://www.youtube.com/channel/UCUPk_7ASBWn_NvQqwJyc8mA'
+    },
+
+    {
+        name: 'Petercraft',
+        link: 'https://www.youtube.com/channel/UCKy57fKpaKYOifflNOMOtUA',
+    },
+
+    {
+        name: 'SkyGamerGR',
+        link: 'https://www.youtube.com/@skygamergrofficial2317'
+    },
+
+    {
+        name: 'Tsismanidis',
+        link: 'https://www.youtube.com/@tsismanidis2140'
+    },
+
+    {
+        name: 'St7rm',
+        link: 'https://youtube.com/@yungpl7gue'
+    }
+];
