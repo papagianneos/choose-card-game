7b5364d787d209c90c53eec853ee0d6833ec1a5e
Sotiris25 <71824952+Sotiris25@users.noreply.github.com>
2023-05-25 15:13:47 +0300

    Add files via upload

---
+++ modules/skins.js
@@ -0,0 +1,22 @@
+export const SKINS_CONFIG = {
+    'no_skin': {
+        name: 'No Skin',
+        id: 'no_skin',
+        bg: 'none',
+        pageBg: 'none',
+    },
+
+    'woody': {
+        name: 'Woody',
+        id: 'woody',
+        bg: 'url(/img/wood.png)',
+        pageBg: 'url(/img/wood_bg.png)'
+    },
+
+    'metal': {
+        name: 'Metallic',
+        id: 'metal',
+        bg: 'url(/img/metal.png)',
+        pageBg: 'url(/img/metal_bg.png)'
+    },
+};
