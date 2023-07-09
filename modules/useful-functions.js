5edca35d8fcb68ff6e00244a6cf58406e8ff5d60
Sotiris25 <71824952+Sotiris25@users.noreply.github.com>
2023-07-09 15:11:28 +0300

    Update useful-functions.js

--- modules/useful-functions.js
+++ modules/useful-functions.js
@@ -1,13 +1,29 @@
+const irandom = i => {
+    let max = Math.floor(i);
+    return Math.floor(Math.random() * (max + 1));
+};
+
+export const randomChoice = arr => {
+    return arr[irandom(arr.length - 1)];
+};
+
+// For Penalty mode.
+export function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 export const generateRandomHexColor = () => {
     const HEX_DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
-    let hexColor = "#"
+    let hexColor = "#";
 
     // ---------------------------------------------------------------------------
     // Επειδή υπάρχει περίπτωση να μην γίνει σωστά το generation χρώματος.
     // ---------------------------------------------------------------------------
     while (hexColor.length < 7) {
-        hexColor += HEX_DIGITS[Math.round(Math.random() * HEX_DIGITS.length)]
+        hexColor += randomChoice(HEX_DIGITS);
     }
     // ---------------------------------------------------------------------------
 
