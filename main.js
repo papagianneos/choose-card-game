04e1021ce0f69a00cbead50418949c5c8e2d099e
papagianneos <71824952+papagianneos@users.noreply.github.com>
2024-02-25 13:40:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1730,12 +1730,16 @@ import { randomChoice, getRandomInt, generateRandomHexColor, createLoader, shuff
                         card.style.background = resetColor;
                         card.innerHTML = PI_EFFECT_LOL ? Math.PI : '​'; // κενό/whitespace
                         card.style.backgroundSize = 'cover';
-                        if (enabledImaginaryUniverse) {
-                            card.style.transform = `rotateZ(${card.imaginaryRotationType}deg)`;
+
+                        if (card.classList.contains('opened')) {
+                            if (enabledImaginaryUniverse) {
+                                card.style.transform = `rotateZ(${card.imaginaryRotationType}deg)`;
+                            }
+                            else {
+                                card.classList.toggle('opened');
+                            }
                         }
-                        else {
-                            card.classList.toggle('opened');
-                        }
+
                         card.removeAttribute('egineclick');
                         if (pgnBirthday) card.style.backgroundSize = '250%';
 
