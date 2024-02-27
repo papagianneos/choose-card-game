3396e4975dc1a55f91632226df3503135baea109
papagianneos <71824952+papagianneos@users.noreply.github.com>
2024-02-27 22:59:02 +0200

    Update events.js

--- modules/events.js
+++ modules/events.js
@@ -2,7 +2,7 @@ import { createSnow } from './snow.js';
 import { SERVER_ADDRESS } from "./SERVER.js";
 
 export const christmasDecorationsEnabled = new Date().getDate() > 11 && new Date().getMonth() == 10; // December 10th+
-export const pgnBirthday = true;//(new Date().getDate() == 15 && new Date().getMonth() == 5) || (new Date().getDate() == 6 && new Date().getMonth() == 7) || (new Date().getDate() == 21 && new Date().getMonth() == 7); // June 15th, August 6th & August 21st (Redacted's Birthday)
+export const pgnBirthday = (new Date().getDate() == 15 && new Date().getMonth() == 5) || (new Date().getDate() == 6 && new Date().getMonth() == 7) || (new Date().getDate() == 21 && new Date().getMonth() == 7); // June 15th, August 6th & August 21st (Redacted's Birthday)
 export const aprilFools = (new Date().getDate() == 1 && new Date().getMonth() == 3) || (new Date().getDate() == 7 && new Date().getMonth() == 1); // April 1st
 export const halloweenTime = new Date().getDate() > 16 && new Date().getMonth() == 9; // Halloween
 export let skinsDisabled = false;
