b31dd9958048a3ff834e62a59a35c71759d8cf4f
papagianneos <71824952+papagianneos@users.noreply.github.com>
2024-03-02 00:11:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1350,10 +1350,16 @@ import { randomChoice, getRandomInt, generateRandomHexColor, createLoader, shuff
                                     const temp = decreaseTimeBy;
                                     decreaseTimeBy = 0;
                                     sounds.freeze.play();
+                                    document.getElementById('cardsHolder').classList.toggle('frozen');
 
                                     setTimeout(() => {
                                         decreaseTimeBy = temp;
                                         sounds.freeze.play();
+                                        const cards = document.querySelectorAll('.card');
+                                        cards.forEach(cardElem => {
+                                            if (cardElem.savedText == specialCardsConfig.freezer.shape) cardElem.classList.toggle('dead');
+                                        });
+                                        document.getElementById('cardsHolder').classList.toggle('frozen');
                                     }, 5e3);
                                 }
                                 break;
@@ -2736,7 +2742,7 @@ import { randomChoice, getRandomInt, generateRandomHexColor, createLoader, shuff
                                 timeLeft += 400;
                                 const cards = document.querySelectorAll('.card');
                                 cards.forEach(cardElem => {
-                                    if (cardElem.savedText == specialCardsConfig.lifesaver.shape) cardElem.style.filter = 'grayscale(1)';
+                                    if (cardElem.savedText == specialCardsConfig.lifesaver.shape) cardElem.classList.toggle('dead');
                                 });
                                 sounds.saved.play();
                                 preventLose = false;
@@ -3109,7 +3115,7 @@ import { randomChoice, getRandomInt, generateRandomHexColor, createLoader, shuff
 
                             const cards = document.querySelectorAll('.card');
                             cards.forEach(cardElem => {
-                                if (cardElem.savedText == specialCardsConfig.lifesaver.shape) cardElem.style.filter = 'grayscale(1)';
+                                if (cardElem.savedText == specialCardsConfig.lifesaver.shape) cardElem.classList.toggle('dead');
                             });
                             sounds.saved.play();
                             preventLose = false;
