0c91523a0520e177f41b509090528bc1c73f0208
papagianneos <71824952+papagianneos@users.noreply.github.com>
2024-03-02 20:31:56 +0200

    Update achievements-page.js

--- achievements/achievements-page.js
+++ achievements/achievements-page.js
@@ -106,7 +106,7 @@ import { christmasDecorationsEnabled, idkSomeFunctionSoItRuns } from "../modules
 
         achievenentWrapper.appendChild(achievementTitle);
         achievenentWrapper.appendChild(achievementDescription);
-        if (!achievement.dontShowProgress) achievenentWrapper.appendChild(progressBarGreySide);
+        if (achievement.requiredProgress) achievenentWrapper.appendChild(progressBarGreySide);
         achievementDivHolder.appendChild(achievenentWrapper);
 
         // Αν δεν είναι τρόπαιο, βάλτο στα επιτεύγματα, αλλιώς στα τρόπαια.
