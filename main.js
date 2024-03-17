8566dd0eb95c27334123276583c4aee468aed06b
papagianneos <71824952+papagianneos@users.noreply.github.com>
2024-03-17 12:47:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1955,7 +1955,7 @@ import { randomChoice, getRandomInt, generateRandomHexColor, createLoader, shuff
                 // Μουσική Credits (Soundimage.org)
                 let musicCredit = document.createElement('h1');
                 musicCredit.style.fontSize = modifiedFontSize;
-                musicCredit.innerHTML = `${LANGUAGE_DATA[LANGUAGE_INDEX].label_music_credit} Petercraft#7530, <a href="https://pixabay.com/">Pixabay.com</a>, <a href="https://soundimage.org/">Soundimage.org</a> & <a href="https://www.soundhelix.com/">Soundhelix.com</a>.`;
+                musicCredit.innerHTML = `${LANGUAGE_DATA[LANGUAGE_INDEX].label_music_credit} Petercraft#7530, DSTechnician, <a href="https://pixabay.com/">Pixabay.com</a>, <a href="https://soundimage.org/">Soundimage.org</a> & <a href="https://www.soundhelix.com/">Soundhelix.com</a>.`;
 
                 // For my friends :)
                 let friendsWebsite = document.createElement('h1');
@@ -2678,43 +2678,43 @@ import { randomChoice, getRandomInt, generateRandomHexColor, createLoader, shuff
                             sounds.angryPgnFinale.play();
                             pageBody.style.transition = '1s';*/
                             //setTimeout(() => {
-                               /* pageBody.style.transform = 'rotate(360deg)';
-                                for (var index = 0; index < 15; index++) {
-                                    createCard({
-                                        shape: 'mazeWall',
-                                        color: 'transparent',
-                                        mazeWall: true,
-                                        specialCard: true,
-                                        specialCardEffect: () => { }
-                                    });
-                                    resetCards(false);
-                                }
+                            /* pageBody.style.transform = 'rotate(360deg)';
+                             for (var index = 0; index < 15; index++) {
+                                 createCard({
+                                     shape: 'mazeWall',
+                                     color: 'transparent',
+                                     mazeWall: true,
+                                     specialCard: true,
+                                     specialCardEffect: () => { }
+                                 });
+                                 resetCards(false);
+                             }
 
-                                // ανακάτεψε τις κάρτες
-                                let cardsListToShuffle = [],
-                                    scoreAndTriesTextHolderChild = parentDiv.children[0];
+                             // ανακάτεψε τις κάρτες
+                             let cardsListToShuffle = [],
+                                 scoreAndTriesTextHolderChild = parentDiv.children[0];
 
-                                // Για κάθε "παιδί" που έχει το cardsHolder/parentDiv
-                                for (var e = 0; e < parentDiv.children.length; e++) {
-                                    let child = parentDiv.children[e];
-                                    // Για να βρούμε ποιο είναι κάρτα και ποιο κείμενο, διαβάζουμε το class του.
-                                    if (child.className.includes('card')) {
-                                        cardsListToShuffle.push(child);
-                                    }
-                                }
+                             // Για κάθε "παιδί" που έχει το cardsHolder/parentDiv
+                             for (var e = 0; e < parentDiv.children.length; e++) {
+                                 let child = parentDiv.children[e];
+                                 // Για να βρούμε ποιο είναι κάρτα και ποιο κείμενο, διαβάζουμε το class του.
+                                 if (child.className.includes('card')) {
+                                     cardsListToShuffle.push(child);
+                                 }
+                             }
 
-                                shuffle(cardsListToShuffle);
+                             shuffle(cardsListToShuffle);
 
-                                cardsListToShuffle[(cardsListToShuffle.length)] = cardsListToShuffle[0];
-                                cardsListToShuffle[0] = scoreAndTriesTextHolderChild;
-                                parentDiv.replaceChildren(...cardsListToShuffle);
+                             cardsListToShuffle[(cardsListToShuffle.length)] = cardsListToShuffle[0];
+                             cardsListToShuffle[0] = scoreAndTriesTextHolderChild;
+                             parentDiv.replaceChildren(...cardsListToShuffle);
 
-                                currentSpecialCards.push('mazeWall');
-                                removedSpecialCardsFromFullCount.push(false);
-                                blockClicks = false;*/
-                                gameMusic = music.papagianneosFinaleMusic;
-                                gameMusic.play();
-                           // }, 2e3);
+                             currentSpecialCards.push('mazeWall');
+                             removedSpecialCardsFromFullCount.push(false);
+                             blockClicks = false;*/
+                            gameMusic = music.papagianneosFinaleMusic;
+                            gameMusic.play();
+                            // }, 2e3);
                         }
 
                         // Αν υπάρχουν λιγότερο από 6 κλειστές κάρτες, βάλε το εφέ
@@ -3073,7 +3073,9 @@ import { randomChoice, getRandomInt, generateRandomHexColor, createLoader, shuff
                             sounds.angryPgnFinale.play();
                             papagianneosFinaleAngryRun = true;
                             pageBody.style.transition = '1s';
+                            $('#cardsHolder').fadeOut(300);
                             setTimeout(() => {
+                                sounds.finaleEnter.play();
                                 challengeModeEnabled = true;
                                 scoreText.innerText = 'YOU WILL NOT WIN';
                                 scoreText.style.color = 'red';
@@ -3156,9 +3158,12 @@ import { randomChoice, getRandomInt, generateRandomHexColor, createLoader, shuff
 
                                 currentSpecialCards.push('mazeWall', '+H+', specialCardsConfig.hammer.shape);
                                 removedSpecialCardsFromFullCount.push(false, false, false);
-                                blockClicks = false;
-                                gameMusic = music.theTrueFinale;
-                                gameMusic.play();
+                                $('#cardsHolder').fadeIn(300);
+                                setTimeout(() => {
+                                    blockClicks = false;
+                                    gameMusic = music.theTrueFinale;
+                                    gameMusic.play();
+                                }, 1e3);
                             }, 2e3);
                         }
                         else if (!voidModeOver) {
